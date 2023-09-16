72f9c3db1e37743d5bbf83954bcee70173d58389
thesayf <r_ori@hotmail.co.uk>
2023-09-16 17:13:17 +0100

    making changes to see if i have correctly taken off auto deploy in netlify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. yooooooo
+          Edit <code>src/App.js</code> and save to reload. This is the new update
         </p>
         <a
           className="App-link"

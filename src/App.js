6b83c4361f2a85d670f7e6015c3e832a55a52d27
thesayf <r_ori@hotmail.co.uk>
2023-09-16 16:35:01 +0100

    changed the yaml to deploy to prod

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. HEEEEEYee
+          Edit <code>src/App.js</code> and save to reload. HEEEEEYeeeeewww
         </p>
         <a
           className="App-link"
